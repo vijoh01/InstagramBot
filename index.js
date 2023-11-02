3e3a10eba4ceca8c9e17deb27b7f8fb58cff46d7
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2023-11-02 19:52:59 +0100

    another acc support

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 var compression = require('compression')
 
+
+const rateLimit = require('express-rate-limit');
+
+
 require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 const { IgApiClient } = require('instagram-private-api');
 
@@ -13,118 +18,150 @@ const app = express();
 
 app.use(express.json());
 
+
+
 const corsOptions = {
-  origin: '', 
+  origin: '',
 };
 
 app.use(cors(corsOptions));
 app.use(compression());
 
 app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'"],
-      imgSrc: ["'self'"],
-      fontSrc: ["'self'"],
-      connectSrc: ["'self'"],
-      objectSrc: ["'none'"],
-    },
-  }));
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    imgSrc: ["'self'"],
+    fontSrc: ["'self'"],
+    connectSrc: ["'self'"],
+    objectSrc: ["'none'"],
+  },
+}));
 
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100, 
+  message: 'Too many requests, please try again later.',
+});
+
+app.use(limiter);
 
 const ig = new IgApiClient();
 
-app.post('/login', async (req, res) => {
+app.post('/login/:id', async (req, res) => {
+  const type = req.params.id;
+
+  const loginUser = async (username, password) => {
     try {
-    ig.state.generateDevice(process.env.IG_USERNAME);
-    ig.state.proxyUrl = process.env.IG_PROXY;
+      ig.state.generateDevice(username);
+      ig.state.proxyUrl = process.env.IG_PROXY;
 
+      await ig.simulate.preLoginFlow();
+      const loggedInUser = await ig.account.login(username, password);
+      console.log(type + ' instagram logged in as', loggedInUser.username);
 
-    await ig.simulate.preLoginFlow();
-    const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-    console.log('Logged in as', loggedInUser.username);
-
-    res.status(201).send({ success: "success" });
+      res.status(201).send({ success: "success" });
     } catch (error) {
-        console.log(error);
-        res.status(201).send({ success: "fail" });
-    }
-});
-
-async function downloadImageAsFile(imageURL) {
-    try {
-      const response = await axios.get(imageURL, { responseType: 'arraybuffer' });
-      if (response.status === 200) {
-        return Buffer.from(response.data);
-      } else {
-        throw new Error('Failed to download the image');
-      }
-    } catch (error) {
-      throw error;
+      console.log(error);
+      res.status(201).send({ success: "fail" });
     }
   }
 
-app.post('/post', async (req, res) => {
-    const getRandomImage = async () => {
-        try {
-            const unsplashAPIKey = process.env.UNSPLASH_API_KEY;
-    
-            const response = await axios.get('https://api.unsplash.com/photos/random', {
-                headers: {
-                    Authorization: `Client-ID ${unsplashAPIKey}`,
-                },
-                params: {
-                    query: 'fitness',
-                    orientation: 'squarish',
-                    stats: 'true'
-                },
-            });
+  switch (type) {
+    case "fitness":
 
-            const imageURL = response.data.urls.regular;
-            return await downloadImageAsFile(imageURL);
-    
-            
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to fetch a random fitness image' });
-        }
-    }
+      await loginUser(process.env.FITNESS_USERNAME, process.env.FITNESS_PASSWORD);
 
-    const getRandomQuote = async () => {
-        try {
-            const apiKey = process.env.NINJA_API_KEY;
-            const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
-                headers: {
-                    'X-Api-Key': `${apiKey}`,
-                  },
-                params: {
-                    category: 'fitness'
-                },
-            });
-           
-            return response.data[0].quote;
-            
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to fetch a random fitness quote' });
-        }
-    }
-    const image = await getRandomImage();
-    const quote = await getRandomQuote();
-    if (image && quote){
+      break;
+    case "exploration":
+    case "life":
 
-    const media = await ig.publish.photo({
-        file: image,
-        caption: quote + '\n\n#Fitness #Workout #Gym #Exercise #Health #FitnessMotivation #HealthyLifestyle #FitLife #GetFit #FitnessGoals #WeightLoss #StrengthTraining #Cardio #Nutrition #FitFam #BodyBuilding #CrossFit #Running #Yoga #OutdoorFitness #Wellness #ActiveLife #FitnessJourney #Muscle #Training #Sweat #Motivation #HealthyChoices #NoExcuses #HealthAndFitness',
-    });
-    console.log('Media uploaded:', media);
-    res.status(201).send({ success: "success" });
-    } else {
-        res.status(201).send({ success: "fail" });
-    }
+      await loginUser(process.env.TRAVEL_USERNAME, process.env.TRAVEL_PASSWORD);
+
+      break;
+    default:
+      res.status(201).send({ success: "fail", message: "Specify type of account" });
+      break;
+  }
 });
 
-app.listen(3000, () => console.log("Server running on port http://localhost:3000/"))
+async function downloadImageAsFile(imageURL) {
+  try {
+    const response = await axios.get(imageURL, { responseType: 'arraybuffer' });
+    if (response.status === 200) {
+      return Buffer.from(response.data);
+    } else {
+      throw new Error('Failed to download the image');
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.post('/post/:id', async (req, res) => {
+  const type = req.body.id;
+
+  const getRandomImage = async () => {
+    try {
+      const unsplashAPIKey = process.env.UNSPLASH_API_KEY;
+
+      const response = await axios.get('https://api.unsplash.com/photos/random', {
+        headers: {
+          Authorization: `Client-ID ${unsplashAPIKey}`,
+        },
+        params: {
+          query: type === 'fitness' ? 'fitness' : (type === 'exploration' || type === 'life') ? 'travel' : 'travel',
+          orientation: 'squarish',
+          stats: 'true'
+        },
+      });
+
+      const imageURL = response.data.urls.regular;
+      return await downloadImageAsFile(imageURL);
+
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch a random fitness image' });
+    }
+  }
+
+  const getRandomQuote = async () => {
+    try {
+      const apiKey = process.env.NINJA_API_KEY;
+      const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
+        headers: {
+          'X-Api-Key': `${apiKey}`,
+        },
+        params: {
+          category: type === 'fitness' ? 'fitness' : type === 'exploration' ? 'exploration' : 'life',
+        },
+      });
+      const data = await response.data[0];
+      return data;
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch a random fitness quote' });
+    }
+  }
+  const image = await getRandomImage();
+  const quote = await getRandomQuote();
+  if (image && quote) {
+
+    await ig.publish.photo({
+      file: image,
+      caption: quote.quote + " - " + quote.author + "\n\n" + (type === 'fitness' ? '#bodygoals #fitfam #fitspo #fitspo #abs #fitness #workoutroutine #gym #gymmotivation #sixpack #fitnessmotivation #gains #workout #workoutmotivation' : '#travel #nature #travelawesome #travelblogger #traveladdict #instatravel #traveling #places #placestovisit #worldplaces #visit #travelinspo #beautifulplaces #AdventureSeeker #TravelPhotography')
+    });
+    console.log('Media uploaded');
+    res.status(201).send({ success: "success" });
+  } else {
+    res.status(201).send({ success: "fail" });
+  }
+});
+
+app.listen(port, () => console.log("Server running on port " + port))
 
 
